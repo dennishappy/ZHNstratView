5d6fb5b9c86ed3e9f64c7808619e012966407b6d
zhnnnnn <475816908@qq.com>
2016-04-26 00:16:15 +0800

    Initial Commit

---
+++ ZHNstratView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHNstratView
+//
+//  Created by zhn on 16/4/26.
+//  Copyright © 2016年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
