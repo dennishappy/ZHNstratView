edc39758e786c599913dec4a904cfd15648dd548
zhnnnnn <475816908@qq.com>
2016-04-27 13:23:38 +0800

    星级评分显示空间

--- ZHNstratView/ViewController.m
+++ ZHNstratView/ViewController.m
@@ -7,16 +7,34 @@
 //
 
 #import "ViewController.h"
-
+#import "singleStarView.h"
+#import "ZHNstarView.h"
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *numLabel;
+@property (nonatomic,weak) ZHNstarView * starView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    singleStarView *startView = [[singleStarView alloc]initWithFrame:CGRectMake(100, 100, 200, 200)];
+    [self.view addSubview:startView];
+    
+    ZHNstarView * newStartView = [[ZHNstarView alloc]initWithFrame:CGRectMake(0, 400, self.view.frame.size.width*0.5, 30)];
+    newStartView.starNumber = 5;
+    newStartView.animate = YES;
+    newStartView.startShowScore = 2.6;
+    newStartView.backFillColor = [UIColor grayColor];
+    newStartView.hightLightFillColor = [UIColor redColor];
+    newStartView.scoreInterger = YES;
+    [self.view addSubview:newStartView];
+    self.starView = newStartView;
+}
+- (IBAction)clickShowButton:(id)sender {
+    self.numLabel.text = [NSString stringWithFormat:@"%.2f",self.starView.currentScore];
+    NSLog(@"%.1f",self.starView.currentScore);
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ ZHNstratView/singleStarView.m
@@ -0,0 +1,95 @@
+//
+//  customStartView.m
+//  ZHNstratView
+//
+//  Created by zhn on 16/4/26.
+//  Copyright © 2016年 zhn. All rights reserved.
+//
+
+#import "singleStarView.h"
+
+@interface singleStarView()
+
+@property (nonatomic,strong) NSMutableArray * outPointsArray;
+@property (nonatomic,strong) NSMutableArray * inPointsArray;
+
+@end
+
+
+
+@implementation singleStarView
+
+- (void)drawRect:(CGRect)rect{
+
+    UIBezierPath * path = [UIBezierPath bezierPath];
+    path.lineWidth = 0.5;
+    if (self.strokeColor) {
+        [self.strokeColor set];
+    }else{
+        [self.fillColor set];
+    }
+    [self.fillColor setFill];
+    for (int i = 0; i < 5; i++) {
+        if (i == 0) {
+            [path moveToPoint:[self.outPointsArray[i]CGPointValue]];
+            [path addLineToPoint:[self.inPointsArray[i]CGPointValue]];
+        }else{
+            [path addLineToPoint:[self.outPointsArray[i]CGPointValue]];
+            [path addLineToPoint:[self.inPointsArray[i]CGPointValue]];
+        }
+    }
+    [path closePath];
+    [path stroke];
+    if (self.isFillStar == YES) {
+        [path fill];
+    }
+}
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        [self initSubViews:frame];
+        self.backgroundColor = [UIColor clearColor];
+    }
+    return self;
+}
+
+
+- (void)initSubViews:(CGRect)frame{
+    [self Get5Points:frame];
+}
+
+
+-(void)Get5Points:(CGRect)frame
+{
+    CGFloat radius = frame.size.width/2;
+    for (NSInteger i = 0; i < 5; i++) {
+        NSInteger delta = ( i == 1 ? frame.size.width / 30 : 0);
+        CGFloat x = radius + radius * cos((0.2 * M_PI * 2)*i+(0.05 * M_PI * 2));
+        CGFloat y = radius - radius * sin((0.2 * M_PI * 2)*i+(0.05 * M_PI *2)) + delta;
+        CGPoint point = CGPointMake(x, y);
+        [self.outPointsArray addObject:[NSValue valueWithCGPoint:point]];
+    }
+    
+    CGFloat smallRadius = radius * sin(0.05 * M_PI * 2)/sin(0.1 * M_PI * 2);
+    for (NSInteger i=0; i<5; i++) {
+        CGFloat x = radius + smallRadius * cos((0.1 * M_PI * 2)*(1 + 2 * i)+(0.05 * M_PI * 2));
+        CGFloat y = radius - smallRadius * sin((0.1 * M_PI * 2)*(1 + 2 * i)+(0.05 * M_PI * 2));
+        [self.inPointsArray addObject:[NSValue valueWithCGPoint:CGPointMake(x, y)]];
+    }
+}
+
+- (NSMutableArray *)outPointsArray{
+    if (_outPointsArray == nil) {
+        _outPointsArray = [NSMutableArray array];
+    }
+    return _outPointsArray;
+}
+- (NSMutableArray *)inPointsArray{
+    if (_inPointsArray == nil) {
+        _inPointsArray = [NSMutableArray array];
+    }
+    return _inPointsArray;
+}
+
+
+@end
